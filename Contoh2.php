ae5d980cab5a13814bdb91cd53ce30a1fda70c2a
RagiLONE <ragilsuryaarrohman@gmail.com>
2022-03-28 21:52:33 +0700

    penambahan isi Contoh_1.php

---
+++ Contoh2.php
@@ -0,0 +1,24 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "Selamat Datang.. selamat belajar Web Programming";
+ }
+}
+
+<?php
+class Model_latihan1 extends CI_Model
+{
+ //membuat variable untuk menampung nilai
+ public $nilai1, $nilai2, $hasil;
+ //method penjumlahan
+ public function jumlah($n1 = null, $n2 = null)
+ {
+33
+ $this->nilai1 = $n1;
+ $this->nilai2 = $n2;
+ $this->hasil = $this->nilai1 + $this->nilai2;
+ return $this->hasil;
+ }
+}
